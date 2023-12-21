efaab2af8e968123834c47b903d2447cd827cfef
Dmitriy Tyutyukov <dmitriy.tyutyukov@ifellow.ru>
2023-12-21 08:27:22 +0300

    changed comment + naming variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        float loanRate = 9.99F;//Процентную ставку
+        float loanRate = 9.99F;//Процентная ставка
         double amountOfCredit = 1_000_000;// Сумма кредита
         int creditTerm = 3;// Максимальный срок кредита в годах
         for (int i = 1; i <= creditTerm; i++) {
